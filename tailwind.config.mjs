5248479b0c2049897eb7bc9883f44a2c7dbfc74f
Jose M BlackCat <josemblackcat@gmail.com>
2024-10-29 11:44:35 -0500

    Nav desktop actualizado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Space Grotesk', 'sans-serif'],
+		},
+		colors: {
+			'primary': '#B9FF66',
+			'secondary': '#191A23',
+			'tertiary': '#F3F3F3',
+		},
 		extend: {},
 	},
 	plugins: [],
